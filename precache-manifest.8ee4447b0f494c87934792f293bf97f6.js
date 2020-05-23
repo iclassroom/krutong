e091826209b06c82cfe80fe7316c03ec75847914
kruanurak <akhophadung@gmail.com>
2020-05-23 22:00:33 +0700

    Updates

---
+++ precache-manifest.8ee4447b0f494c87934792f293bf97f6.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e93f1231ea0352fa3547bc5b249197a7",
+    "url": "/krutong/index.html"
+  },
+  {
+    "revision": "f3794f0642bbf25afacb",
+    "url": "/krutong/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "9ddf710d6c2ae4a700e2",
+    "url": "/krutong/static/js/2.1bab1f98.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/krutong/static/js/2.1bab1f98.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f3794f0642bbf25afacb",
+    "url": "/krutong/static/js/main.6f56b99e.chunk.js"
+  },
+  {
+    "revision": "67a186e29426b7cf0b4e",
+    "url": "/krutong/static/js/runtime-main.a5e6d86d.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/krutong/static/media/logo.5d5d9eef.svg"
+  }
+]);
